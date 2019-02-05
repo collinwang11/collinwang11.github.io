6e107b64e15435508bc788c9b6d4f878e3329038
Collin Wang <zhiqinwang11@gmail.com>
2019-02-05 03:03:15 -0500

    Add files via upload

---
+++ head.js
@@ -0,0 +1,48 @@
+
+$(document).ready(function() {
+    $('.nav_1').click(function(){
+        $("#Home").show();
+        $("#Education").hide();
+        $("#Experience").hide();
+        $("#Projects").hide();
+    });
+});
+$(document).ready(function() {
+    $('.nav_2').click(function(){
+       $("#Home").hide();
+        $("#Education").show();
+        $("#Experience").hide();
+        $("#Projects").hide();
+    });
+});
+$(document).ready(function() {
+    $('.nav_3').click(function(){
+       $("#Home").hide();
+        $("#Education").hide();
+        $("#Experience").show();
+        $("#Projects").hide();
+    });
+});
+$(document).ready(function() {
+    $('.nav_4').click(function(){
+       $("#Home").hide();
+        $("#Education").hide();
+        $("#Experience").hide();
+        $("#Projects").show();
+    });
+});
+
+$(document).ready(function() {
+    $('.dropdown').click(function(){
+       $("#Home").show();
+        $("#Education").hide();
+        $("#Experience").hide();
+        $("#Projects").hide();
+    });
+});
+
+$(document).ready(function() {
+    $('.nav_10').click(function(){
+		$("#Home").show();
+	})
+});
